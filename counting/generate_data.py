2b823ac240c777cb81e478379ba61cf0a38b0616
Alexandre Vallette <vallettea@gmail.com>
2015-07-02 12:39:49 +0200

    regression converges to mean

--- counting/generate_data.py
+++ counting/generate_data.py
@@ -12,30 +12,32 @@ import glob
 
 FIG_NUMBER = 100
 MAX_POINTS = 10
-FIG_SIZE = 100
-REC_HEI = 5
-REC_WID = 5
-my_dpi = 100
+FIG_SIZE = 10
+REC_HEI = 1
+REC_WID = 1
 
 output = []
 if not os.path.exists("../data/counting_samples"):
     os.makedirs("../data/counting_samples")
 
 for num_fig in range(FIG_NUMBER):
-	plt.figure(figsize=(FIG_SIZE/my_dpi, FIG_SIZE/my_dpi), dpi=my_dpi)
-	currentAxis = plt.gca()
+	fig = plt.figure(frameon=False)
+	fig.set_size_inches(FIG_SIZE,FIG_SIZE)
+	ax = plt.Axes(fig, [0., 0., 1., 1.])
+	ax.set_axis_off()
+	fig.add_axes(ax)
 	# take random number of point between 
 	npts = random.randint(0, MAX_POINTS);
 	for i in range(npts):
 		x = int(np.floor(FIG_SIZE * random.random()))
 		y = int(np.floor(FIG_SIZE * random.random()))
-		currentAxis.add_patch(Rectangle((x, y), REC_WID, REC_HEI, fill=True, alpha=1, color='#000000', edgecolor='#000000'))
+		ax.add_patch(Rectangle((x, y), REC_WID, REC_HEI, fill=True, alpha=1, color='#000000', edgecolor='#000000'))
 
 	plt.xlim([0, FIG_SIZE])
 	plt.ylim([0, FIG_SIZE])
 	plt.axis('off')
 	plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
-	plt.savefig("../data/counting_samples/" + str(num_fig) + ".png")
+	plt.savefig("../data/counting_samples/" + str(num_fig) + ".png", dpi=1)
 	output += [[str(num_fig) + ".png", str(npts)]]
 
 
--- counting/train.js
+++ counting/train.js
@@ -1,22 +1,17 @@
 "use strict";
+require('es6-shim');
 
 var convnetjs = require("convnetjs");
 var fs = require('fs');
 var csv = require('csv-parser');
 var getPixels = require("get-pixels");
-var glob = require("glob");
 
 //// PARAMETERS
-var N_TRAIN = 400;
-var ITER = 400;
-var num_batches = 51; // 20 training batches, 1 test
-var test_batch = 50;
-var num_samples_per_batch = 1000;
-var image_dimension = 100;
+var N_TRAIN = 90;
+var ITER = 100;
+var MAX_POINTS = 10;
+var image_dimension = 10;
 var image_channels = 1;
-var use_validation_data = true;
-var random_flip = true;
-var random_position = true;
 
 // define layers
 var layer_defs = [];
@@ -28,7 +23,8 @@ layer_defs.push({type:'conv', sx:5, filters:20, stride:1, pad:2, activation:'rel
 layer_defs.push({type:'pool', sx:2, stride:2});
 // layer_defs.push({type:'conv', sx:5, filters:20, stride:1, pad:2, activation:'relu'});
 // layer_defs.push({type:'pool', sx:2, stride:2});
-layer_defs.push({type:'softmax', num_classes:10});
+// layer_defs.push({type:'softmax', num_classes:MAX_POINTS});
+layer_defs.push({type:'regression', num_neurons:1});
 
 // error window
 var Window = function(size, minsize) {
@@ -66,90 +62,73 @@ var trainer = new convnetjs.SGDTrainer(net, {method:'adadelta', batch_size:4, l2
 var formatValues = function(file, number) {
 
   var pathImage = "../data/counting_samples/" + file;
-  console.log("Analyse image :", pathImage);
   var image;
   
-  getPixels(pathImage, function(err, pixels) {
-    if(err) {
-      console.log(err);
-      return
-    }
-    console.log("got pixels", pixels.shape.slice())
-    image = pixels
+  return new Promise(function(resolve, reject){
+    // we may use https://github.com/gchudnov/inkjet for jpeg
+    getPixels(pathImage, function(err, pixels) {
+      if(err) {
+        console.log(err);
+        reject(err);
+      }
+      image = pixels;
+      var imageData = [];
+      for (var i=0; i< image.data.length; i+=4){
+        imageData.push(image.data[i.toString()])
+      }
+
+      var x = new convnetjs.Vol(imageData);
+      // target
+      var y = [parseFloat(number)];
+
+      resolve({x : x, y : y});
+    })
   })
-
-  console.log("TEST", image);
-
-  var imageData = listPixel.map(function(i){ return image.data[i]});  
-
-  var x = new convnetjs.Vol(imageData);
-  // target
-  var y = [parseFloat(row[number])]; // this must be a list
-
-  return {x : x, y : y};
 }
 
 
-var train = [];
-var test = [];
+var trainP = [];
+var testP = [];
 
 var lossWindow = new Window(N_TRAIN);
 var k = 0;
 var square_error = 0.0
 
 
-var nombre = image_dimension * image_dimension;
-var listPixel = [];
-
-// create list of pixel
-for (var iters=0; iters<nombre; iters = iters + 4) {
-  listPixel.push(iters.toString());
-}
 
 
-var fs = require('fs');
 fs.createReadStream("../data/counting.csv")
   .pipe(csv({separator: ','}))
   .on('data', function(data) {
-    formatValues(data.file, data.number)
-});
+    if (k < N_TRAIN)
+      trainP.push(formatValues(data.file, data.number));
+    else
+      testP.push(formatValues(data.file, data.number));
+    k++;
+  })
+  .on("end", function(){
+    console.log("Starting training");
+    Promise.all(trainP).then(function(train){
+      //train
+      for(var iters=0; iters<ITER; iters++) {
+        train.forEach(function(row){
+          var stats = trainer.train(row.x, row.y);
+          lossWindow.add(stats.loss);
+        })
+        if (iters % 10 === 0) 
+          console.log("step ", iters, " on ", ITER, ' loss', lossWindow.get_average());
+      }
 
-
-// glob("../data/counting_samples/*png", function (er, imagePath) {
-//   formated(imagePath)
-// })
-// fs.createReadStream("data/boston.csv")
-//   .pipe(csv({separator: ','}))
-//   .on('data', function(data) {
-//     var formated = formatValues(data);
-//     if (k < N_TRAIN){
-//       train.push(formated);
-//         k++;
-//       } else {
-//         test.push(formated);
-//       }
-//   })
-//   .on("end", function(){
-
-//     //train
-
-//     for(var iters=0; iters<ITER; iters++) {
-//       train.forEach(function(row){
-//         var stats = trainer.train(row.x, row.y);
-//         lossWindow.add(stats.loss);
-//       })
-//       if (iters % 10 === 0) 
-//         console.log("step ", iters, " on ", ITER, ' loss', lossWindow.get_average());
-//     }
-
-//     //testing
-
-//     test.forEach(function(v){
-//       var predicted = net.forward(v.x);
-//       var error = predicted.w[0] - v.y;
-//       square_error += error * error;
-//       console.log('Predicted', predicted.w[0], 'Expected', v.y);
-//     })
-//     console.log("MSE", square_error / test.length);
-//     console.log("Finished")
-//   });
+      //test
+      Promise.all(testP).then(function(test){
+        test.forEach(function(v){
+          var predicted = net.forward(v.x);
+          var error = predicted.w[0] - v.y;
+          square_error += error * error;
+          console.log('Predicted', predicted.w[0], 'Expected', v.y);
+        })
+        console.log("MSE", square_error / test.length);
+        console.log("Finished")
+      });
+    });
+  });
